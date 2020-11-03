216f6ceb155f962a79e721f791fdb9153d481149
SusanLiu93 <liuss>
2020-11-03 15:57:24 +0800

    初建vue3项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+ hello world vue3
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
